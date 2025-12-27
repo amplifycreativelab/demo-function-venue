738797885e46454febe42cc31d3c74bb493eda58
Spaghetti <stefanomocci92@gmmail.com>
2025-12-27 22:39:50 +0800

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://github.com/amplifycreativelab",
-  base: "/function-venue-demo/",
+  site: "https://amplifycreativelab.github.io",
+  base: "/demo-function-venue",
   integrations: [sitemap()]
 });
